7467e48671b58dd83449d23767bf6674bbaf3348
winter-han1 <541451968@qq.com>
2019-12-28 09:59:11 +0800

    pass

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,17 @@
 public class FizzBuzz {
 
     public static void main(String[] args) {
-
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    System.out.println("fizzbuzz");
+                }
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
