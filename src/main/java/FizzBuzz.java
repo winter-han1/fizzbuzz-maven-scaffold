3e4f5d29623ed0cbaa17569c91e49f60aaa6460d
winter-han1 <541451968@qq.com>
2019-12-28 12:03:07 +0800

    pass

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,6 +2,9 @@ public class FizzBuzz {
 
     public static String of(int number) {
         if (number % 3 == 0) {
+            if (number % 5 == 0) {
+                return "FizzBuzz";
+            }
             return "Fizz";
         } else if (number % 5 == 0) {
             return "Buzz";
