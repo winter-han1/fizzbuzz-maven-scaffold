7b9ed6638d3f1387cedf5a72165a32e3c356a44d
winter-han1 <541451968@qq.com>
2019-12-28 12:15:34 +0800

    refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,9 @@
 public class FizzBuzz {
 
     public static void main(String[] args) {
-
+        for (int i = 1; i < 101; i++) {
+            System.out.println(of(i));
+        }
     }
 
     public static String of(int number) {
