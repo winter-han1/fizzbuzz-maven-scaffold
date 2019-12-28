d6d823f30597618440a9a5a64f8d8d5dfe2f3d9d
winter-han1 <541451968@qq.com>
2019-12-28 11:30:43 +0800

    red

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+@RunWith(Parameterized.class)
+public class FizzBuzzTest {
+
+    private int number;
+    private String expectedResult;
+
+    public FizzBuzzTest(int number, String expectedResult) {
+        this.number = number;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {1, "1"},
+                {3, "Fizz"},
+                {5, "Buzz"},
+                {15, "FizzBuzz"}
+        });
+    }
+
+    @Test
+    public void test() {
+        assertThat(FizzBuzz.of(number)).isEqualTo(expectedResult);
+    }
+}
